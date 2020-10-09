38bab48c932fa2dd0614e193ac53da77857b644e
Josh8903 <thorngren87@gmail.com>
2020-10-09 02:25:52 -0500

    animated storm cloud, implemented the thing

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-    $("#cloud").click(function() {
+    $(".cloud").click(function() {
         var audio = $("#storm")[0];
         if (audio.paused) {
             audio.play();
+            $(this).attr("id", "true");
         } else {
             audio.pause();
+            $(this).attr("id", "false");
         }
-
     });
 });
